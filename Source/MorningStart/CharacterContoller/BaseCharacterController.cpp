dd9a1876ea31ad19c335418836e6c7b3f15cedf6
GuyTaieb <guytaieb414@gmail.com>
2024-03-23 11:02:44 +0200

    Cleanup character

--- Source/MorningStart/CharacterContoller/BaseCharacterController.cpp
+++ Source/MorningStart/CharacterContoller/BaseCharacterController.cpp
@@ -20,11 +20,10 @@ DEFINE_LOG_CATEGORY(LogBaseTemplateCharacter);
 //////////////////////////////////////////////////////////////////////////
 // AMorningStartCharacter
 
-ABaseCharacterController::ABaseCharacterController()
-{
+ABaseCharacterController::ABaseCharacterController() {
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
-		
+
 	// Don't rotate when the controller rotates. Let that just affect the camera.
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
@@ -51,7 +50,8 @@ ABaseCharacterController::ABaseCharacterController()
 
 	// Create a follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	// Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
 	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
@@ -61,20 +61,17 @@ ABaseCharacterController::ABaseCharacterController()
 //////////////////////////////////////////////////////////////////////////
 // Input
 
-void ABaseCharacterController::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
+void ABaseCharacterController::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
 	// Add Input Mapping Context
-	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
-	{
-		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
-		{
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController())) {
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<
+			UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer())) {
 			Subsystem->AddMappingContext(DefaultMappingContext, 0);
 		}
 	}
-	
+
 	// Set up action bindings
 	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) {
-		
 		// Jumping
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
@@ -84,27 +81,26 @@ void ABaseCharacterController::SetupPlayerInputComponent(UInputComponent* Player
 
 		// Looking
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABaseCharacterController::Look);
-	}
-	else
-	{
-		UE_LOG(LogBaseTemplateCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
+	} else {
+		UE_LOG(LogBaseTemplateCharacter, Error,
+		       TEXT(
+			       "'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."
+		       ), *GetNameSafe(this));
 	}
 }
 
-void ABaseCharacterController::Move(const FInputActionValue& Value)
-{
+void ABaseCharacterController::Move(const FInputActionValue& Value) {
 	// input is a Vector2D
 	FVector2D MovementVector = Value.Get<FVector2D>();
 
-	if (Controller != nullptr)
-	{
+	if (Controller != nullptr) {
 		// find out which way is forward
 		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
 
 		// get forward vector
 		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-	
+
 		// get right vector 
 		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 
@@ -114,17 +110,17 @@ void ABaseCharacterController::Move(const FInputActionValue& Value)
 	}
 }
 
-void ABaseCharacterController::Look(const FInputActionValue& Value)
-{
+void ABaseCharacterController::Look(const FInputActionValue& Value) {
 	// input is a Vector2D
 	FVector2D LookAxisVector = Value.Get<FVector2D>();
 
-	if (Controller != nullptr){
+	if (Controller != nullptr) {
 		// add yaw and pitch input to controller
 		AddControllerYawInput(LookAxisVector.X);
 		AddControllerPitchInput(LookAxisVector.Y);
 	}
 }
+
 // Called when the game starts or when spawned
 void ABaseCharacterController::BeginPlay() {
 	Super::BeginPlay();
@@ -138,4 +134,3 @@ void ABaseCharacterController::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 	StateMachine->Update(DeltaTime);
 }
-
